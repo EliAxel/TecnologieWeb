42a130e187feaae29102d3e1f9cf407cf54d05d3
EliAxel <eliamartinelli75@gmail.com>
2025-06-02 11:24:43 +0200

    build pulita

--- progetto_tw/sylvelius/tests.py
+++ progetto_tw/sylvelius/tests.py
@@ -84,19 +84,19 @@ class AnonUrls(TestCase):
         self.assertEqual(response.status_code, 405)
     
     def test_pagamento_ok(self):
-        response = self.client.get('/pagamento_ok/')
+        response = self.client.get('/pagamento/confermato/')
         self.assertEqual(response.status_code, 200)
 
     def test_pagamento_nonok(self):
-        response = self.client.get('/pagamento_nonok/')
+        response = self.client.get('/pagamento/annullato/')
         self.assertEqual(response.status_code, 200)
 
     def test_paypal_pcc(self):
-        response = self.client.get('/paypal/pcc/')
+        response = self.client.get('/pagamento/paypal/pcc/')
         self.assertEqual(response.status_code, 405)
     
     def test_paypal_coa(self):
-        response = self.client.get('/paypal/coa/')
+        response = self.client.get('/pagamento/paypal/coa/')
         self.assertEqual(response.status_code, 405)
 
 class LoggedUrls(TestCase):
